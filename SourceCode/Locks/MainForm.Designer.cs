d67dad7d4fc283c39c0c5d4b6eee05e5c4d3ab8f
Christophe Nasarre <c.nasarresoulier@criteo.com>
2019-09-16 11:18:55 +0200

    Add deadlock code sample

---
+++ SourceCode/Locks/MainForm.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Locks
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDeadlock = new System.Windows.Forms.Button();
+            this.tbResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnDeadlock
+            // 
+            this.btnDeadlock.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDeadlock.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.btnDeadlock.Location = new System.Drawing.Point(13, 13);
+            this.btnDeadlock.Name = "btnDeadlock";
+            this.btnDeadlock.Size = new System.Drawing.Size(391, 48);
+            this.btnDeadlock.TabIndex = 0;
+            this.btnDeadlock.Text = "Start dead(good)lock";
+            this.btnDeadlock.UseVisualStyleBackColor = true;
+            this.btnDeadlock.Click += new System.EventHandler(this.btnDeadlock_Click);
+            // 
+            // tbResult
+            // 
+            this.tbResult.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbResult.Enabled = false;
+            this.tbResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.tbResult.Location = new System.Drawing.Point(13, 68);
+            this.tbResult.Multiline = true;
+            this.tbResult.Name = "tbResult";
+            this.tbResult.Size = new System.Drawing.Size(391, 53);
+            this.tbResult.TabIndex = 1;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(416, 133);
+            this.Controls.Add(this.tbResult);
+            this.Controls.Add(this.btnDeadlock);
+            this.Name = "MainForm";
+            this.Text = "Locks";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDeadlock;
+        private System.Windows.Forms.TextBox tbResult;
+    }
+}
+
---
+++ SourceCode/Locks/MainForm.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Locks
+{
+    public partial class MainForm : Form
+    {
+        private readonly ManualResetEvent _continueDeadlock;
+        private readonly object _lock1 = new Object();
+        private readonly object _lock2 = new Object();
+        private int _resource1 = 0;
+        private int _resource2 = 0;
+        private bool _lockStarted = false;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            _continueDeadlock = new ManualResetEvent(false);
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            Text = $"pid = {Process.GetCurrentProcess().Id}";
+
+            ThreadPool.QueueUserWorkItem(OnAccessResource, new object[] { 1, _lock1, _lock2 });
+            ThreadPool.QueueUserWorkItem(OnAccessResource, new object[] { 2, _lock2, _lock1 });
+        }
+
+        private void btnDeadlock_Click(object sender, EventArgs e)
+        {
+            if (_lockStarted)
+            {
+                _continueDeadlock.Reset();
+                _lockStarted = false;
+                btnDeadlock.Text = "Start dead(good)lock";
+            }
+            else
+            {
+                _continueDeadlock.Set();
+                _lockStarted = true;
+                btnDeadlock.Text = "Stop dead(good)lock";
+            }
+        }
+
+        private void OnAccessResource(object state)
+        {
+            Random r = new Random();
+
+            while (true)
+            {
+                _continueDeadlock.WaitOne();
+
+                object[] parameters = (object[])state;
+                int id = (int)parameters[0];
+
+                lock (parameters[1])
+                {
+                    _resource1++;
+                    lock (parameters[2])
+                    {
+                        _resource2++;
+
+                        this.BeginInvoke(
+                            (Action)(() =>
+                            {
+                                tbResult.Text = $"[{id}] - {_resource1} | {_resource2}";
+                            })
+                        );
+                        Thread.Sleep((int)(500 + r.NextDouble() * 1000));
+                    }
+                }
+            }
+        }
+
+    }
+}
