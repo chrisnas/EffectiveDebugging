1a5a35c56ef984aaca3791a549ffd9eded7bdc3a
Kevin Gosse <krix33@gmail.com>
2019-09-28 18:47:05 +0200

    Set the name of the cat threads

--- SourceCode/StockMarket/MainWindow.xaml.cs
+++ SourceCode/StockMarket/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -79,6 +80,8 @@ namespace StockMarket
         private void CatThread(object state)
         {
             (var cat, var rnd) = (ValueTuple<Cat, Random>)state;
+
+            Thread.CurrentThread.Name = $"Cat {cat.Name}";
 
             var perceivedValue = new Dictionary<string, int>();
 
